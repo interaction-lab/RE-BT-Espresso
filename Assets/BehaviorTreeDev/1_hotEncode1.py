a0e0c1c4edf66a0c6077c88550ac5bc7ec02d1bf
Adam Wathieu <ajw135@georgetown.edu>
2020-08-08 11:31:10 -0400

    Partially refactored. Still working on it

--- Assets/BehaviorTreeDev/1_hotEncode1.py
+++ Assets/BehaviorTreeDev/1_hotEncode1.py
@@ -5,81 +5,87 @@ import numpy as np
 import argparse
 import os
 import json
+import pipeline_constants as constants
+from json_manager import json_manager
 
-le = LabelEncoder()
+CATEGORICAL_NULL_VALUE = "No Entry"
 
-def getHotEncodedHeader(hE_object, originalColumnNames):
-	headerList = []
-	index = 0
-	for category in hE_object:
-		originalColumn = originalColumnNames[index]
+def get_hot_encoded_header(hot_encoder, categorical_features):
+	header_list = []
+	for index, category in enumerate(hot_encoder.categories_):
+		original_column_name = categorical_features[index]
 		for column in category:
-			headerList.append(originalColumn + "_" + column)
-		index += 1
-	return headerList
+			header_list.append(original_column_name + "_" + column)
+	return header_list
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-config", "--configuration", required = True, help = "Path to the configuration file")
-args = vars(ap.parse_args())
-json_file_path = args["configuration"]
-# opening JSON file
-json_object = json.load(open(json_file_path))
-directory_in_str = json_object["normalized_path"]
-directory = os.fsencode(directory_in_str)
+def process_command_line_args():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-config", "--configuration", required = True, help = "Path to the configuration file")
+	args = vars(ap.parse_args())
+	return args["configuration"]
 
-fileName = os.fsdecode(os.path.join(directory_in_str, "combined_csv.csv"))
+def main():
+	json_file_path = process_command_line_args()
+	JSON_MANAGER = json_manager(json_file_path)
+	feature_columns = JSON_MANAGER.get_feature_columns()
+	categorical_features = JSON_MANAGER.get_categorical_features()
+	hot_encoded_path = JSON_MANAGER.get_hot_encoded_path()
 
-features_data = pd.read_csv(fileName, usecols = json_object["feature_columns"])
+	normalized_folder = os.fsdecode(os.path.join(JSON_MANAGER.get_normalized_path(), constants.NORMALIZED_CSV_FOLDER_NAME))
+	combined_csv_file = os.fsdecode(os.path.join(normalized_folder, constants.COMBINED_CSV_FILE_NAME))
 
-for column in json_object["categorical_features"]:
-	features_data[column] = features_data[column].astype(str)
-	features_data[column].fillna("No Entry", inplace = True)
+	features_data = pd.read_csv(combined_csv_file, usecols = feature_columns)
 
-enc = OneHotEncoder()
-enc.fit(features_data[json_object["categorical_features"]])
-hE_header = getHotEncodedHeader(enc.categories_, json_object["categorical_features"])
+	for feature in categorical_features:
+		features_data[feature].fillna(CATEGORICAL_NULL_VALUE, inplace = True)
+		features_data[feature] = features_data[feature].astype(str)
+		
+	hot_encoder = OneHotEncoder()
+	hot_encoder.fit(features_data[categorical_features])
+	hot_encoded_header = get_hot_encoded_header(hot_encoder, categorical_features)
+	hot_encoded_array = hot_encoder.transform(features_data[categorical_features]).toarray()
 
-fmt = ""
-for i in range(len(hE_header)):
-	fmt += "%i,"
+	features_data = features_data.drop(columns = categorical_features)
+	features_data_array = features_data.to_numpy()
 
-hE_array = enc.transform(features_data[json_object["categorical_features"]]).toarray()
-features_data = features_data.drop(columns = json_object["categorical_features"])
-features_data_columnNames = list(features_data.columns)
-for i in range(len(features_data_columnNames)):
-	fmt += "%1.3f,"
-fmt += "%i"
-features_data = features_data.to_numpy()
+	final_csv = np.append(hot_encoded_array, features_data_array, axis = 1)
 
-final_csv = np.append(hE_array, features_data, axis=1)
+	le = LabelEncoder()
 
-labels_data = pd.read_csv(fileName, usecols = ['Label'])
-le.fit(labels_data[['Label']].values.ravel())
-labels_data = np.array(le.transform(labels_data[['Label']].values.ravel()))
+	labels_data = pd.read_csv(combined_csv_file, usecols = [constants.LABEL_COLUMN_NAME])
+	le.fit(labels_data[[constants.LABEL_COLUMN_NAME]].values.ravel())
+	labels_data = np.array(le.transform(labels_data[[constants.LABEL_COLUMN_NAME]].values.ravel()))
 
-labels_reformatted = []
-for label in labels_data: labels_reformatted.append([int(label)])
+	labels_reformatted = []
+	for label in labels_data: labels_reformatted.append([int(label)])
 
-final_csv = np.append(final_csv, labels_reformatted, axis=1)
+	final_csv = np.append(final_csv, labels_reformatted, axis=1)
 
-final_Header = ""
-for element in hE_header:
-	final_Header += element + ","
-for element in features_data_columnNames:
-	final_Header += element + ","
-final_Header += "Label"
+	hot_encode_fmt = "%i," * len(hot_encoded_header) #format hot encoded column to ints
+	feature_data_fmt = "%1.3f," * len(list(features_data.columns))
+	total_fmt = hot_encode_fmt + feature_data_fmt + "%i" # for label
 
+	final_header = ""
+	for element in hot_encoded_header:
+		final_header += element + ","
+	for element in list(features_data.columns):
+		final_header += element + ","
+	final_header += constants.LABEL_COLUMN_NAME
 
-if not os.path.isdir(json_object["hotEncoded_path"]): os.mkdir(json_object["hotEncoded_path"])
-hot_encoded_path = os.path.join(json_object["hotEncoded_path"], "hotEncoded_combined.csv")
-if os.path.exists(hot_encoded_path): os.remove(hot_encoded_path)
+	if not os.path.isdir(hot_encoded_path): os.mkdir(hot_encoded_path)
+	hot_encoded_path = os.fsdecode(os.path.join(hot_encoded_path, constants.HOT_ENCODED_CSV_FILE_NAME))
+	if os.path.exists(hot_encoded_path): os.remove(hot_encoded_path)
 
-np.savetxt(os.fsdecode(hot_encoded_path), final_csv, fmt=fmt, header = final_Header, delimiter=",", comments='')
+	np.savetxt(hot_encoded_path, final_csv, fmt = total_fmt, header = final_header, delimiter = constants.CSV_DELIMITER, comments='')
 
 
-outputLogFile = "output.log"
+	outputLogFile = "output.log"
 
-f = open(outputLogFile, "w")
-f.write("{}\n".format(fmt))
-f.write(str(list(le.inverse_transform(range(len(le.classes_))))))
-f.close()
+	f = open(outputLogFile, "w")
+	f.write("{}\n".format(total_fmt))
+	f.write(str(list(le.inverse_transform(range(len(le.classes_))))))
+	f.close()
+
+if __name__ == '__main__':
+	main()
+
