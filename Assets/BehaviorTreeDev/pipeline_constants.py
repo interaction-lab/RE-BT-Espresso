c2ac430aeeae22a557dda1b6a44e2d679630beea
Adam Wathieu <ajw135@georgetown.edu>
2020-08-11 10:12:10 -0400

    further refactoring, added the folder function to the constants file

--- Assets/BehaviorTreeDev/pipeline_constants.py
+++ Assets/BehaviorTreeDev/pipeline_constants.py
@@ -1,10 +1,27 @@
+import os
+
 NORMALIZED_CSV_FOLDER_NAME = "normalized_CSVs"
-COMBINED_CSV_FILE_NAME = "combined_csv.csv"
+COMBINED_CSV_FILENAME = "combined_csv.csv"
 
 HOT_ENCODED_CSV_FOLDER_NAME = "hotEncoded_CSVs"
-HOT_ENCODED_CSV_FILE_NAME = "hotEncoded_combined.csv"
+HOT_ENCODED_CSV_FILENAME = "hotEncoded_combined.csv"
+
+UPSAMPLED_CSV_FOLDER_NAME = "upsampled_CSVs"
+UPSAMPLED_CSV_FILENAME = "upsampled_combined.csv"
 
 CSV_DELIMITER = ','
 CSV_QUOTECHAR = '"'
 
 LABEL_COLUMN_NAME = "Label"
+
+COLUMN_AXIS = 1
+
+# Description: creates and adds folder called folder_name to directory: working_directory,
+#			   returns path to folder
+# ex: folder_name = "normalizedCSVs", working_directory = /path/to/directory
+# return: "/path/to/directory/normalizedCSVs"
+def add_folder_to_directory(folder_name, working_directory):
+	new_directory = os.fsdecode(os.path.join(working_directory, folder_name))
+	if not os.path.isdir(new_directory): 
+		os.makedirs(new_directory)
+	return new_directory
