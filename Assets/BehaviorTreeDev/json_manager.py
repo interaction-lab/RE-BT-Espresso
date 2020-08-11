e9e1c7d96d0d86bea9e13d845afc3aa70ca4ba40
Adam Wathieu <ajw135@georgetown.edu>
2020-08-11 10:11:42 -0400

    add more functions, moved everything from json_constants.py to here

--- Assets/BehaviorTreeDev/json_manager.py
+++ Assets/BehaviorTreeDev/json_manager.py
@@ -1,5 +1,16 @@
 import json
-import json_constants as json_elements
+
+JSON_CSV_PATH = "csv_folder_path"
+JSON_NORMALIZED_PATH = "normalized_path"
+JSON_HOT_ENCODED_PATH = "hotEncoded_path"
+JSON_UPSAMPLED_PATH = "upSampled_path"
+JSON_FEATURE_COLUMNS = "feature_columns"
+JSON_LABEL_COLUMNS = "label_columns"
+JSON_LAG_FEATURES = "lag_features"
+JSON_SLIDING_WINDOW_LENGTH = "sliding_window_length"
+JSON_CATEGORICAL_FEATURES = "categorical_features"
+JSON_RANDOM_STATE = "random_state"
+JSON_UPSAMPLE = "upsampling"
 
 class json_manager:
 
@@ -9,25 +20,34 @@ class json_manager:
 		self.v_json_object = json.load(open(json_file_path))
 
 	def get_feature_columns(self):
-		return self.v_json_object[json_elements.JSON_FEATURE_COLUMNS]
+		return self.v_json_object[JSON_FEATURE_COLUMNS]
 
 	def get_label_columns(self):
-		return self.v_json_object[json_elements.JSON_LABEL_COLUMNS]
+		return self.v_json_object[JSON_LABEL_COLUMNS]
 
 	def get_lag_features(self):
-		return list(self.v_json_object[json_elements.JSON_LAG_FEATURES])
+		return list(self.v_json_object[JSON_LAG_FEATURES])
 
 	def get_csv_path(self):
-		return str(self.v_json_object[json_elements.JSON_CSV_PATH])
+		return str(self.v_json_object[JSON_CSV_PATH])
 
 	def get_normalized_path(self):
-		return str(self.v_json_object[json_elements.JSON_NORMALIZED_PATH])
+		return str(self.v_json_object[JSON_NORMALIZED_PATH])
+
+	def get_upsampled_path(self):
+		return str(self.v_json_object[JSON_UPSAMPLED_PATH])
 
 	def get_sliding_window_length(self):
-		return int(self.v_json_object[json_elements.JSON_SLIDING_WINDOW_LENGTH])
+		return int(self.v_json_object[JSON_SLIDING_WINDOW_LENGTH])
 
 	def get_categorical_features(self):
-		return list(self.v_json_object[json_elements.JSON_CATEGORICAL_FEATURES])
+		return list(self.v_json_object[JSON_CATEGORICAL_FEATURES])
 
 	def get_hot_encoded_path(self):
-		return str(self.v_json_object[json_elements.JSON_HOT_ENCODED_PATH])
+		return str(self.v_json_object[JSON_HOT_ENCODED_PATH])
+
+	def get_random_state(self):
+		return self.v_json_object[JSON_RANDOM_STATE]
+
+	def get_upsample_status(self):
+		return self.v_json_object[JSON_UPSAMPLE]
