ab323c699d7fd4ed2324bce40d2ef6a46235666f
Adam Wathieu <ajw135@georgetown.edu>
2020-08-11 10:09:56 -0400

    incremental refactoring, close to being done

--- Assets/BehaviorTreeDev/2_upsample1.py
+++ Assets/BehaviorTreeDev/2_upsample1.py
@@ -6,45 +6,50 @@ import numpy as np
 import os
 import argparse
 import json
+from json_manager import json_manager
+import pipeline_constants as constants
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-config", "--configuration", required = True, help = "Path to the configuration file")
-ap.add_argument("-outputLog", "--outputLogFile", required = True, help = "Path to log file")
-args = vars(ap.parse_args())
-json_file_path = args["configuration"]
-fmt_file_path = args["outputLogFile"]
-# opening JSON file
-json_object = json.load(open(json_file_path))
-directory_in_str = json_object["hotEncoded_path"]
-directory = os.fsencode(directory_in_str)
+def process_command_line_args():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-config", "--configuration", required = True, help = "Path to the configuration file")
+	ap.add_argument("-outputLog", "--outputLogFile", required = True, help = "Path to log file")
+	args = vars(ap.parse_args())
+	return args["configuration"], args["outputLogFile"]
 
-fileName = os.fsdecode(os.path.join(directory_in_str, "hotEncoded_combined.csv"))
+def main():
+	json_file_path, fmt_file_path = process_command_line_args()
+	JSON_MANAGER = json_manager(json_file_path)
 
-f = open(fmt_file_path, "r")
-fmt = f.readline()
-# labelEncoding = input()
+	if JSON_MANAGER.get_upsample_status() == True:
+		upsampled_path = JSON_MANAGER.get_upsampled_path()
+		hot_encoded_folder = os.fsdecode(os.path.join(JSON_MANAGER.get_hot_encoded_path(), constants.HOT_ENCODED_CSV_FOLDER_NAME))
+		hot_encoded_file = os.fsdecode(os.path.join(hot_encoded_folder, constants.HOT_ENCODED_CSV_FILENAME))
 
+		hotEncoded_data = pd.read_csv(hot_encoded_file)
+		# print("hotEncoded_data.columns: {}".format(list(hotEncoded_data.columns)))
+		numCols = len(hotEncoded_data.columns)
+		numCols_list = list(range(numCols))
 
-hotEncoded_data = pd.read_csv(fileName)
-numCols = len(hotEncoded_data.columns)
-numCols_list = list(range(numCols))
-
-header = ""
-for element in list(hotEncoded_data.columns):
-	header += element + ","
-header = header[:-1]
-
-
-features_data = pd.read_csv(fileName, usecols = numCols_list[:-1])
-labels_data = pd.read_csv(fileName, usecols = [numCols_list[-1]])
-
-sm = SVMSMOTE(random_state = 0)
-X_res, y_res = sm.fit_resample(features_data, labels_data)
-csv_ready = np.append(X_res, y_res, axis=1)
-
-if not os.path.isdir(json_object["upSampled_path"]): os.mkdir(json_object["upSampled_path"])
-upSampled_path = os.path.join(json_object["upSampled_path"], "upSampled_combined.csv")
-if os.path.exists(upSampled_path): os.remove(upSampled_path)
+		features_data = pd.read_csv(hot_encoded_file, usecols = numCols_list[:-1])
+		labels_data = pd.read_csv(hot_encoded_file, usecols = [numCols_list[-1]])
 
 
-np.savetxt(os.fsdecode(upSampled_path), csv_ready, fmt=fmt, delimiter=",", header = header, comments='')
+		sm = SVMSMOTE(random_state = JSON_MANAGER.get_random_state())
+		X_res, y_res = sm.fit_resample(features_data, labels_data)
+		csv_ready = np.append(X_res, y_res, axis = constants.COLUMN_AXIS)
+
+		upsampled_folder = constants.add_folder_to_directory(constants.UPSAMPLED_CSV_FOLDER_NAME, upsampled_path)
+		upsampled_file_path = os.fsdecode(os.path.join(upsampled_folder, constants.UPSAMPLED_CSV_FILENAME))
+		if os.path.exists(upsampled_file_path): 
+			os.remove(upsampled_file_path)
+
+		f = open(fmt_file_path, "r")
+		fmt = f.readline()
+		f.close()
+
+		header = ','.join(str(i) for i in hotEncoded_data.columns)
+		np.savetxt(upsampled_file_path, csv_ready, fmt = fmt, delimiter = constants.CSV_DELIMITER, header = header, comments='')
+
+if __name__ == '__main__':
+	main()
+
