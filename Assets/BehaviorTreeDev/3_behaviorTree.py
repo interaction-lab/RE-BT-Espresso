585f14febb69605f9c4fef0c7f0b4b31df9fdf87
Adam Wathieu <ajw135@georgetown.edu>
2020-08-11 18:22:26 -0400

    changed some variable names to meet PEP8 standards

--- Assets/BehaviorTreeDev/3_behaviorTree.py
+++ Assets/BehaviorTreeDev/3_behaviorTree.py
@@ -5,15 +5,13 @@ from sklearn.compose import make_column_transformer
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import os
 import json
 import behaviorTree_Builder as btBuilder
-from json_manager import json_manager
+from json_manager import JsonManager
 import pipeline_constants as constants
 
 PRUNING_GRAPH_FILENAME = "accuracy_vs_alpha.png"
@@ -32,28 +30,28 @@ def process_command_line_args():
 
 def main():
 	json_file_path, log_file_path = process_command_line_args()
-	JSON_MANAGER = json_manager(json_file_path)
+	json_manager = JsonManager(json_file_path)
 
 	log_file = open(log_file_path, "r")
 	fmt = log_file.readline()
-	label_encoding = json.loads(log_file.readline())
+	label_encoding = eval(log_file.readline())
 	log_file.close()
 
 	supervised_learning_data = None
-	if JSON_MANAGER.get_upsample_status() == True:
-		upsampled_folder = os.fsdecode(os.path.join(JSON_MANAGER.get_upsampled_path(), constants.UPSAMPLED_CSV_FOLDER_NAME))
+	if json_manager.get_upsample_status() == True:
+		upsampled_folder = os.fsdecode(os.path.join(json_manager.get_upsampled_path(), constants.UPSAMPLED_CSV_FOLDER_NAME))
 		supervised_learning_data = os.fsdecode(os.path.join(upsampled_folder, constants.UPSAMPLED_CSV_FILENAME))
 	else:
-		hot_encoded_folder = os.fsdecode(os.path.join(JSON_MANAGER.get_hot_encoded_path(), constants.HOT_ENCODED_CSV_FOLDER_NAME))
+		hot_encoded_folder = os.fsdecode(os.path.join(json_manager.get_hot_encoded_path(), constants.HOT_ENCODED_CSV_FOLDER_NAME))
 		supervised_learning_data = os.fsdecode(os.path.join(hot_encoded_folder, constants.HOT_ENCODED_CSV_FILENAME))
 
 	supervised_learning_dataframe = pd.read_csv(supervised_learning_data)
 	features_data = pd.read_csv(supervised_learning_data, usecols = list(supervised_learning_dataframe.columns)[:-1])
 	labels_data = pd.read_csv(supervised_learning_data, usecols = [list(supervised_learning_dataframe.columns)[-1]])
 
-	kFold = JSON_MANAGER.get_kfold()
-	max_depth = JSON_MANAGER.get_decision_tree_depth()
-	output_folder = constants.add_folder_to_directory(constants.OUTPUT_FOLDER_NAME, JSON_MANAGER.get_output_path())
+	kFold = json_manager.get_kfold()
+	max_depth = json_manager.get_decision_tree_depth()
+	output_folder = constants.add_folder_to_directory(constants.OUTPUT_FOLDER_NAME, json_manager.get_output_path())
 	folder_name = "{}_kFold_{}_maxDepth".format(kFold, max_depth)
 	output_full_path = constants.add_folder_to_directory(folder_name, output_folder)
 
@@ -66,7 +64,7 @@ def main():
 		X_train, X_test = features_data.iloc[train_index], features_data.iloc[test_index]
 		y_train, y_test = labels_data.iloc[train_index], labels_data.iloc[test_index]
 
-		clf = tree.DecisionTreeClassifier(random_state = JSON_MANAGER.get_random_state(), max_depth = max_depth)
+		clf = tree.DecisionTreeClassifier(random_state = json_manager.get_random_state(), max_depth = max_depth)
 		clf = clf.fit(X_train, y_train)
 
 		trains_accu.append(clf.score(X_train, y_train))
@@ -88,7 +86,7 @@ def main():
 	report_file_obj.write("	Decision Tree (PDF format) saved to: {}.pdf\n".format(dot_pdf_header))
 	report_file_obj.write("Check {} for appropriate pruning.\n\n\n".format(PRUNING_GRAPH_FILENAME))
 
-	clf = tree.DecisionTreeClassifier(random_state = JSON_MANAGER.get_random_state(), max_depth = max_depth)
+	clf = tree.DecisionTreeClassifier(random_state = json_manager.get_random_state(), max_depth = max_depth)
 	clf = clf.fit(features_data, labels_data)
 	dot_pdf_full_path = os.fsdecode(os.path.join(output_full_path, dot_pdf_header))
 	plot_decision_tree(clf, dot_pdf_full_path, features_data.columns)
@@ -102,7 +100,7 @@ def main():
 	clfs = []
 	train_scores = []
 	for i, ccp_alpha in enumerate(ccp_alphas):
-		clf = tree.DecisionTreeClassifier(random_state = JSON_MANAGER.get_random_state(), max_depth = max_depth, ccp_alpha=ccp_alpha)
+		clf = tree.DecisionTreeClassifier(random_state = json_manager.get_random_state(), max_depth = max_depth, ccp_alpha=ccp_alpha)
 		clf.fit(features_data, labels_data)
 		score = clf.score(features_data, labels_data)
 
