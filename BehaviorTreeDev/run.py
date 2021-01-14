0b8753d5419eb6b72bf9e7cc23796b8c7cdbaacf
tgroechel <tgroeche@umich.edu>
2021-01-13 21:48:24 -0800

    Added run.py argparse for path

--- BehaviorTreeDev/run.py
+++ BehaviorTreeDev/run.py
@@ -8,6 +8,9 @@ Attributes:
     output_file_path (str): Description
     upsample_2 (TYPE): Description
 """
+
+import argparse
+
 normalize_0 = __import__('0_normalize1')
 hotencode_1 = __import__('1_hotEncode1')
 upsample_2 = __import__('2_upsample1')
@@ -15,6 +18,12 @@ behaviortree_3 = __import__('3_behaviorTree')
 
 json_file_path = "config.json"
 output_file_path = "output.log"
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--path", required = False, help = "Path to json config")
+args = vars(ap.parse_args())
+json_file_path = args["path"]
+
 
 def main():
 	"""Summary
