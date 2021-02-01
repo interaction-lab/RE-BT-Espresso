b50b32968d9704310c3c45be4c2c5b085ebe7fab
Chloe <cmkuo@usc.edu>
2021-02-01 01:55:19 -0800

    Build tree any tree from robot_config

--- DataSim/tree_units.py
+++ DataSim/tree_units.py
@@ -55,140 +55,23 @@ class Tree_Basic(Tree):
         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
         
     def define_tree(self):
-        if self.type == "selector":
-            self.root = pt.composites.Selector(name="selector")
-        elif self.type == "sequence":
-            self.root = pt.composites.Sequence(name="sequence")
-        elif self.type == "parallel":
-            self.root = pt.composites.Parallel(name="parallel")
-        
-        for child in self.child_list:
-            if child["inverted"]:
-                self.root.add_child(pt.decorators.Inverter(name="inverted_"+child["name"], child=self.behavior_dict[child["name"]]))
-            else:
-                self.root.add_child(self.behavior_dict[child["name"]])
-            
+        self.root = self.recursive_tree_build(self.type, self.child_list)
         self.b_tree = pt.trees.BehaviourTree(self.root)
-
-# '''Selector: 3 actions'''
-# class Tree_Selector_1(Tree):
-#     def __init__(self, p_SenseCorrect, p_ActCorrect):
-#         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
-    
-#     def define_tree(self):
-#         self.child_1_act = New_exercise_dialogue(self.p_ActCorrect["New_Exercise_Dialogue"])
-#         self.child_2_act = Correct_submission_dialogue(self.p_ActCorrect["Correct_Submission_Dialogue"])
-#         self.child_3_act = Hint_dialogue(self.p_ActCorrect["Hint_Dialogue"])
         
-#         self.root = pt.composites.Selector(name="selector", children= [
-#             self.child_1_act,
-#             self.child_2_act,
-#             self.child_3_act
-#         ])
+    def recursive_tree_build(self, type, c_list):
+        if type == "selector":
+            composite = pt.composites.Selector(name="selector")
+        elif type == "sequence":
+            composite = pt.composites.Sequence(name="sequence")
+        elif type == "parallel":
+            composite = pt.composites.Parallel(name="parallel")
         
-#         self.b_tree = pt.trees.BehaviourTree(self.root)
-                
-        
-# '''Selector: 2 actions, 1 inverted action'''
-# class Tree_Selector_2(Tree):
-#     def __init__(self, p_SenseCorrect, p_ActCorrect):
-#         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
-    
-#     def define_tree(self):
-#         self.child_1_act = Correct_submission_dialogue(self.p_ActCorrect["Correct_Submission_Dialogue"])
-#         self.child_2_act = Hint_dialogue(self.p_ActCorrect["Hint_Dialogue"])
-#         self.child_3_act = pt.decorators.Inverter(name="invert_ExerciseSubmissionResult", child=Check_ExerciseSubmissionResult(self.p_SenseCorrect["Check_ExerciseSubmissionResult"]))
-        
-#         self.root = pt.composites.Sequence(name="sequence", children= [
-#             self.child_1_act,
-#             self.child_2_act,
-#             self.child_3_act
-#         ])
-        
-#         self.b_tree = pt.trees.BehaviourTree(self.root)
-        
-# '''Selector: 1 action, 1 sequence (3 actions)'''
-
-        
-# '''Sequence: 3 actions'''
-# class Tree_Sequence_1(Tree):
-#     def __init__(self, p_SenseCorrect, p_ActCorrect):
-#         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
-    
-#     def define_tree(self):
-#         self.child_1_act = New_exercise_dialogue(self.p_ActCorrect["New_Exercise_Dialogue"])
-#         self.child_2_act = Correct_submission_dialogue(self.p_ActCorrect["Correct_Submission_Dialogue"])
-#         self.child_3_act = Hint_dialogue(self.p_ActCorrect["Hint_Dialogue"])
-        
-#         self.root = pt.composites.Sequence(name="sequence", children= [
-#             self.child_1_act,
-#             self.child_2_act,
-#             self.child_3_act
-#         ])
-        
-#         self.b_tree = pt.trees.BehaviourTree(self.root)
-        
-#         #pt.display.render_dot_tree(self.root)
-        
-        
-# '''Sequence: 2 actions, 1 inverted action'''
-# class Tree_Sequence_2(Tree):
-#     def __init__(self, p_SenseCorrect, p_ActCorrect):
-#         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
-    
-#     def define_tree(self):
-#         self.child_2_act = Correct_submission_dialogue(self.p_ActCorrect["Correct_Submission_Dialogue"])
-#         self.child_3_act = Hint_dialogue(self.p_ActCorrect["Hint_Dialogue"])
-#         self.child_1_act = pt.decorators.Inverter(name="invert_ExerciseSubmissionResult", child=Check_ExerciseSubmissionResult(self.p_SenseCorrect["Check_ExerciseSubmissionResult"]))
-        
-#         self.root = pt.composites.Sequence(name="sequence", children= [
-#             self.child_1_act,
-#             self.child_2_act,
-#             self.child_3_act
-#         ])
-        
-#         self.b_tree = pt.trees.BehaviourTree(self.root)
-        
-#         #pt.display.render_dot_tree(self.root)
-
-
-# '''Parallel: 3 actions'''
-# class Tree_Parallel_1(Tree):
-#     def __init__(self, p_SenseCorrect, p_ActCorrect):
-#         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
-    
-#     def define_tree(self):
-#         self.child_1_act = New_exercise_dialogue(self.p_ActCorrect["New_Exercise_Dialogue"])
-#         self.child_2_act = Correct_submission_dialogue(self.p_ActCorrect["Correct_Submission_Dialogue"])
-#         self.child_3_act = Hint_dialogue(self.p_ActCorrect["Hint_Dialogue"])
-        
-#         self.root = pt.composites.Parallel(name="parallel", policy=pt.common.ParallelPolicy.SuccessOnOne(), children= [
-#             self.child_1_act,
-#             self.child_2_act,
-#             self.child_3_act
-#         ])
-        
-#         self.b_tree = pt.trees.BehaviourTree(self.root)
-        
-#         #pt.display.render_dot_tree(self.root)
-        
-        
-# '''Parallel: 2 actions, 1 inverted action'''
-# class Tree_Parallel_2(Tree):
-#     def __init__(self, p_SenseCorrect, p_ActCorrect):
-#         super().__init__(p_SenseCorrect=p_SenseCorrect, p_ActCorrect=p_ActCorrect)
-    
-#     def define_tree(self):
-#         self.child_2_act = Correct_submission_dialogue(self.p_ActCorrect["Correct_Submission_Dialogue"])
-#         self.child_3_act = Hint_dialogue(self.p_ActCorrect["Hint_Dialogue"])
-#         self.child_1_act = pt.decorators.Inverter(name="invert_ExerciseSubmissionResult", child=Check_ExerciseSubmissionResult(self.p_SenseCorrect["Check_ExerciseSubmissionResult"]))
-        
-#         self.root = pt.composites.Parallel(name="parallel", policy=pt.common.ParallelPolicy.SuccessOnOne(), children= [
-#             self.child_1_act,
-#             self.child_2_act,
-#             self.child_3_act
-#         ])
-        
-#         self.b_tree = pt.trees.BehaviourTree(self.root)
-        
-#         #pt.display.render_dot_tree(self.root)
+        for child in c_list:
+            if child["inverted"]:
+                composite.add_child(pt.decorators.Inverter(name="inverted_"+child["name"], child=self.behavior_dict[child["name"]]))
+            elif child["type"] == "composite":
+                composite.add_child(self.recursive_tree_build(child["name"], child["child_list"]))
+            else:
+                composite.add_child(self.behavior_dict[child["name"]])
+        return composite
+            
