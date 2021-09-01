9d6a3635c8615de0ee49df7669a5f9ffbad278d3
Tom Groechel <tgroeche@umich.edu>
2021-09-01 10:33:01 -0700

    Runs from simulated config #82

--- run_experiments.py
+++ run_experiments.py
@@ -24,16 +24,11 @@ should_recolor = False
 
 def parse_args():
 	ap = argparse.ArgumentParser()
-	# TODO: make -c be the experiment to run or something
-	ap.add_argument("-c", "--config", required = True, help = "Full path to json config file for pipeline, relative paths work as well")
+	# TODO: make -c be the experiment folder to run or something
 	ap.add_argument("-r", "--recolor", required = False, action='store_true', help = "Run recoloring of all trees")
 	args = vars(ap.parse_args())
-	json_file_path = ""
-	if "config" in args and args["config"] != None:
-		json_file_path = args["config"]
 	should_recolor = "recolor" in args and args["recolor"] != None and args['recolor']
-
-	return json_file_path, should_recolor
+	return should_recolor
 
 def main():
 	"""Runs the simulator and full pipeline end to end
@@ -43,18 +38,18 @@ def main():
 	#TODO: will loop folders later
 	sim_config = "DataSim/configs/experiments/expr0.json"
 	base_pipeline_config = "DataSim/configs/base_pipeline_config.json"
-	tmp_csv_json_output_path = "output/expr0_config.json"
-	# JasonManager.
 
-	# "csv_folder_path"
-	sim_data_output_path, tree_name = bt_sim.run_sim(sim_config)
-	jm = JsonManager(base_pipeline_config)
-	jm.set_csv_path(sim_data_output_path)
-	jm.write_out_json_to_file(tmp_csv_json_output_path)
-	#json_file_path, should_recolor = parse_args()
-	json_file_path = tmp_csv_json_output_path 
-	should_recolor = False
-	run_pipeline.run_pipeline(json_file_path, should_recolor)
+	should_recolor = parse_args()
+	sim_data_output_path, tree_name = bt_sim.run_sim(sim_config)	
+	pipeline_config_path = write_pipeline_config(base_pipeline_config, sim_data_output_path)
+	run_pipeline.run_pipeline(pipeline_config_path, should_recolor)
+
+def write_pipeline_config(base_pipeline_config, sim_data_output_path):
+    jm = JsonManager(base_pipeline_config)
+    jm.set_csv_path(sim_data_output_path)
+    pipeline_config_path = sim_data_output_path + "pipeline_config.json"
+    jm.write_out_json_to_file(pipeline_config_path)
+    return pipeline_config_path
 
 if __name__ == '__main__':
 	main()
