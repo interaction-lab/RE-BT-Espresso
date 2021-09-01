3dc8c2b63973ec8c92a0e4407e1aec2afd0c0171
Tom Groechel <tgroeche@umich.edu>
2021-09-01 10:50:43 -0700

    #82 loops, need to do dif output folders

--- run_experiments.py
+++ run_experiments.py
@@ -1,6 +1,7 @@
 # global imports
 import argparse
 import sys, os
+import glob
 
 # relative file paths
 sim_folder = "DataSim"
@@ -24,25 +25,38 @@ should_recolor = False
 
 def parse_args():
 	ap = argparse.ArgumentParser()
-	# TODO: make -c be the experiment folder to run or something
+	ap.add_argument("-c", "--config", required = False, help = "Name of experiment config json, without this flag it will recurse though all experiments")
 	ap.add_argument("-r", "--recolor", required = False, action='store_true', help = "Run recoloring of all trees")
 	args = vars(ap.parse_args())
+	json_file_path = None
+	if "config" in args and args["config"] != None:
+		json_file_path = args["config"]
 	should_recolor = "recolor" in args and args["recolor"] != None and args['recolor']
-	return should_recolor
 
+	return json_file_path, should_recolor
 def main():
 	"""Runs the simulator and full pipeline end to end
 	'-c, --config' - [optional] Path to json config
 	"""
 	print("Start Experiments")
-	#TODO: will loop folders later
-	sim_config = "DataSim/configs/experiments/expr0.json"
-	base_pipeline_config = "DataSim/configs/base_pipeline_config.json"
+	global experiments_folder
+	base_pipeline_config = "DataSim/configs/base_pipeline_config.json" # this should not be moved
 
-	should_recolor = parse_args()
-	sim_data_output_path, tree_name = bt_sim.run_sim(sim_config)	
+	single_experiment_filename, should_recolor = parse_args()
+	if single_experiment_filename:
+		run_experiment(base_pipeline_config, experiments_folder + "/" + single_experiment_filename, should_recolor)
+	else:
+		run_all_experiments(base_pipeline_config, should_recolor)
+
+def run_all_experiments(base_pipeline_config, should_recolor):
+    for config_file in glob.glob(experiments_folder + "/*.json"):
+       run_experiment(base_pipeline_config, config_file, should_recolor)
+
+def run_experiment(base_pipeline_config, experiment_file, should_recolor):
+	sim_data_output_path, tree_name = bt_sim.run_sim(experiment_file)	
 	pipeline_config_path = write_pipeline_config(base_pipeline_config, sim_data_output_path)
 	run_pipeline.run_pipeline(pipeline_config_path, should_recolor)
+	
 
 def write_pipeline_config(base_pipeline_config, sim_data_output_path):
     jm = JsonManager(base_pipeline_config)
