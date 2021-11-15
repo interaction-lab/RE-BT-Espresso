7c94bc2d71272981eb51cdfe722436c2fc99edbc
Tom Groechel <tgroeche@umich.edu>
2021-11-15 15:49:13 +0100

    Globals pulled out

--- BehaviorTreeDev/BehaviorTreeBuilder.py
+++ BehaviorTreeDev/BehaviorTreeBuilder.py
@@ -7,24 +7,8 @@ import pyeda
 from pyeda.inter import *
 from pyeda.boolalg.expr import _LITS
 import pipeline_constants as constants
+from BTBuilderGlobals import *
 
-
-# globals #######
-
-# [variable_symbol] -> condition
-lat_cond_lookup = dict()
-# [lat_action] -> prior action
-act_to_lat_sets_dict = dict() 
-# set of all binary features
-binary_feature_set = set()
-# Counter for number of variables processed, used for unique naming
-var_cycle_count = 0
-# Used to give unique names to selector/sequence/inverter nodes to avoid stars
-node_name_counter = 0
-# Number of cycle nodes, used for unique naming
-cycle_node_counter = 0
-# [action][lat_action] -> conditions that came with lat minus lat cond
-act_lat_conditions_dict = dict() 
 
 def find_max_index(numpy_1D_array):
     """Finds and returns first max argument index in numpy array
